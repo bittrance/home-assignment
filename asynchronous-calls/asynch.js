0ccc3525d4a3629cabb079c226f75df77f527fa4
Quest <quest@windwards.net>
2016-05-11 00:38:50 +0200

    Version asynchronous calls exercise filling requirements a, b and c

---
+++ asynchronous-calls/asynch.js
@@ -0,0 +1,39 @@
+var start = new Date().getTime();
+
+var asynchronousOperation = function(callback) { 
+  var t = Math.floor(Math.random() * 500 + 2000);
+  var now = new Date().getTime()
+  setTimeout(function() {
+    console.log("t=" + t + ", d=" + (now - start));
+    callback();
+  }, t); 
+}; 
+ 
+var run = function(n, m) {
+  var operationDone = function() {
+    console.log('OK');
+  };
+ 
+  var initial, queue = Math.max(0, n - m);
+  
+  var next = function() {
+    if(queue > 0) {
+      // There are more in the queue
+      queue--;
+      asynchronousOperation(next);
+    } else {
+      // No unstarted jobs left; am I last?
+      initial--;
+      if(initial == 0) {
+        // I was last
+        operationDone();
+      }
+    }
+  }
+  
+  for(initial = 0; initial < Math.min(m, n); initial++) {
+    asynchronousOperation(next);
+  }
+  console.log("Started " + n + " asynchronous operations");
+}
+run(12, 3);
